9d0afef155aee409108bfee90b3fe56b4347e58f
Krzysztof <99942553+KrzysztofA@users.noreply.github.com>
2024-02-20 22:47:57 +0100

    Simplified testing tool
    Removed for loop added equation

--- Test/TestUtils/FileListCreator.py
+++ Test/TestUtils/FileListCreator.py
@@ -6,12 +6,9 @@ import os
 class FileListCreator:
     def __init__(self, root_folder: str = "./Tests", nest_level: int = 1, folders_number: int = 1, file_name: str = "test", files_number: int = 1):
         self.file_list: List[str] = []
-        no: int = 0
-        for i in range(1, nest_level + 1):
-            no += math.pow(folders_number, i)
-        no = int(no)
+        num: int = int((1-math.pow(folders_number, nest_level + 1))/(1-folders_number)) - 1
 
-        for i in range(0, no):
+        for i in range(0, num):
             for j in range(0, files_number):
                 if i == 0:
                     self.file_list.append(os.path.join(root_folder, f"{file_name}_{j}.txt"))
